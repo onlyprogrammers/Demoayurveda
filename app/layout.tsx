3c7a225fda58d8ee08723bcda6be309f363b703c
onlyprogrammers <toajeetrathore@gmail.com>
2025-06-04 08:03:06 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className + " bg-green-300"} >
+      <body className={inter.className + " bg-white"} >
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <div className="flex min-h-screen flex-col">
             <Navbar />
